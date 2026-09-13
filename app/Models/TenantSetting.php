259af0a2a872926d2bdbd91a644d35a9d6d87f51
atefAkl <atefakl80@gmail.com>
2026-09-13 08:34:24 +0300

    fix(settings): add static get and set helper methods to TenantSetting and AdminSetting models

--- app/Models/TenantSetting.php
+++ app/Models/TenantSetting.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class TenantSetting extends Model
 {
     protected $fillable = ['key', 'value'];
+
+    public static function get(string $key, $default = null)
+    {
+        $setting = static::where('key', $key)->first();
+        return $setting ? $setting->value : $default;
+    }
+
+    public static function set(string $key, $value): void
+    {
+        static::updateOrCreate(['key' => $key], ['value' => $value]);
+    }
 }
